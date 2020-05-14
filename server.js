1d5945e7cab330e70464c99d37907bc64c8d5efc
bsihame <sihamebazi@pursuit.org>
2020-05-14 09:32:10 -0400

    add login POST route

--- server.js
+++ server.js
@@ -1,28 +1,46 @@
 const express = require("express");
 const app = express();
 
-const monckUserData = [
-  {name:'Mark'},
-  {name:'Jill'}
-]
+const monckUserData = [{ name: "Mark" }, { name: "Jill" }];
 
 app.get("/user", function (req, res) {
-  res.json({
-    success: true,
-    message: "successfully got user. Nice!",
-    users: monckUserData
-  })
-})
+	res.json({
+		success: true,
+		message: "successfully got user. Nice!",
+		users: monckUserData,
+	});
+});
 
 app.get("/user/:id", function (req, res) {
-  console.log(req.params.id)
-  res.json({
-    success: true,
-    message: "got one user",
-    user: req.params.id
-  })
-})
+	console.log(req.params.id);
+	res.json({
+		success: true,
+		message: "got one user",
+		user: req.params.id,
+	});
+});
 
-app.listen(8000, function (){
-  console.log("server is running")
-})
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const monckUsername = "billyTheKid";
+  const monckPassword = "superSecret"
+  
+  if (username === monckUsername && password === monckPassword) {
+    res.json({
+      success: true,
+      message: "password and username match! ",
+      token: "encrypted token goes here"
+    })
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match"
+    })
+  }
+});
+
+app.listen(8000, function () {
+	console.log("server is running");
+});
