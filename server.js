f7f8348aa336109aa56ba549b9ebd35fe15e80a1
bsihame <sihamebazi@pursuit.org>
2020-05-14 09:00:00 -0400

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const monckUserData = [
+  {name:'Mark'},
+  {name:'Jill'}
+]
+
+app.get("/user", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got user. Nice!",
+    users: monckUserData
+  })
+})
+
 app.listen(8000, function (){
   console.log("server is running")
 })
